e13b262bac944c0dbeb1f001468a80e80537b3bf
Yonara Brito <yonaradebritopereira@gmail.com>
2024-07-28 20:34:12 -0300

    corrigindo erro

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('Site do youtube', () => {
   it('Validar título do site', () => {
-    cy.visit('https://www.youtube.com/')
-    cy.title().should('eq', 'YouTube')
+    cy.visit('https://sauter.digital/')
+    cy.title().should('eq', 'Sauter: Estratégias digitais | Soluções Inovadoras')
   })
 
 })
