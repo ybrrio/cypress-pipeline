7fd912dfc3438ec51fef3c5e8f8f2d658d5927e3
Yonara Brito <yonaradebritopereira@gmail.com>
2024-07-28 21:29:48 -0300

    nova atualização

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-describe('Site do youtube', () => {
+describe('Site da Sauter Digital', () => {
   it('Validar título do site', () => {
     cy.visit('https://sauter.digital/')
     cy.title().should('eq', 'Sauter: Estratégias digitais | Soluções Inovadoras')
